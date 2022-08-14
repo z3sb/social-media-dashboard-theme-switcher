f5d818ef1435ffe74427d952ebcbb79fb840a69b
Haider Fadel <oslvuao@gmail.com>
2022-08-14 18:41:55 +0300

    Upload Files

---
+++ script.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+  body.className = localStorage.getItem('theme')
+})
+const button = document.getElementById('btn');
+const body = document.body;
+button.addEventListener('click', () => {
+  if (body.className === 'light'){
+    body.className = 'dark';
+    localStorage.setItem('theme', 'dark');
+  } else{
+    body.className = 'light';
+    localStorage.setItem('theme', 'light');
+  }
+})
